7aab84225a43d9bc4379ae677d9b15c30d6da98e
lskaxiefam <zeekillingmachine98@gmail.com>
2021-06-27 18:27:56 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -230,9 +230,9 @@ var main = {
       rating = 'is-danger'
     } else if (item.rate < minRate) {
       rating = 'is-warning'
-    } else if (item.rate < idealRate) {
+    } else if (item.rate <= idealRate) {
       rating = 'is-info';
-    } else if (item.rate > idealRate) {
+    } else if (item.rate >= idealRate) {
       rating = 'is-success';
     }
      
